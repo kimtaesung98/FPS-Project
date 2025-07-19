e02c43e791475cd4a0a52f7b6786aaa680c7f59b
ablehun0302 <ablehun@naver.com>
2025-07-19 15:04:44 +0900

    156. 애니메이션

--- Hyper_FPS/Assets/Scripts/Weapon.cs
+++ Hyper_FPS/Assets/Scripts/Weapon.cs
@@ -5,6 +5,8 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] int damage = 1;
     [SerializeField] ParticleSystem muzzleFlashParticle;
+    [SerializeField] Animator animator;
+        const string SHOOT_ANIM = "Shoot";
 
     StarterAssetsInputs starterAssetsInputs;
 
@@ -18,13 +20,13 @@ public class Weapon : MonoBehaviour
         if (!starterAssetsInputs.shoot) return;
 
         muzzleFlashParticle.Play();
+        animator.Play(SHOOT_ANIM, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
 
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
             hit.collider.GetComponent<EnemyHealth>()?.TakeDamage(damage);
         }
-
-        starterAssetsInputs.ShootInput(false);
     }
 }
