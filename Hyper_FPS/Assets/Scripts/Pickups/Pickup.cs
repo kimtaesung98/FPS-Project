de68d81c5d7fb972b856cee3fc8465c42c6612da
ablehun0302 <ablehun@naver.com>
2025-08-13 14:45:35 +0900

    170. 상속을 이용한 기본 픽업
    Pickup abstract class 를 만들고 다른 픽업 클래스에서 상속하여 픽업 구현

---
+++ Hyper_FPS/Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public abstract class Pickup : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 100f;
+
+    protected const string PLAYER_STRING = "Player";
+
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(PLAYER_STRING))
+        {
+            ActiveWeapon activeWeapon = other.GetComponentInChildren<ActiveWeapon>();
+            OnPickup(activeWeapon);
+            Destroy(gameObject);
+        }
+    }
+
+    protected abstract void OnPickup(ActiveWeapon activeWeapon);
+}
