212315cc30fe365772b6f2e289bb9c20190e6315
ablehun0302 <ablehun@naver.com>
2025-08-18 16:53:37 +0900

    179. 데스 카메라
    메인 씬에 가상 데스장면 카메라를 추가하여 사망 시 데스카메라로 전환

--- Hyper_FPS/Assets/Scripts/Player/PlayerHealth.cs
+++ Hyper_FPS/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,14 @@
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int startingHealth = 5;
+    [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
+    [SerializeField] Transform weaponCamera;
 
     int currentHealth;
+    int deathCameraPriority = 20;
 
     void Awake()
     {
@@ -18,6 +22,8 @@ public class PlayerHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            weaponCamera.parent = null; // WeaponCamera를 플레이어로부터 분리 -> 카메라가 사라지지 않도록 함
+            deathVirtualCamera.Priority = deathCameraPriority; // 죽으면 DeathCamera의 우선순위를 높여서 보이도록 함
             Destroy(this.gameObject);
         }
     }
