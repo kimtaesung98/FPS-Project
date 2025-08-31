d6cdcfb59a97f8693c1ccd2b2ec7ec89ef24d0c0
ablehun0302 <ablehun@naver.com>
2025-08-31 16:05:27 +0900

    185. 게임오버 화면
    게임오버 시 게임오버 화면이 켜지도록 함

--- Hyper_FPS/Assets/Scripts/Player/PlayerHealth.cs
+++ Hyper_FPS/Assets/Scripts/Player/PlayerHealth.cs
@@ -9,6 +9,7 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
     [SerializeField] Transform weaponCamera;
     [SerializeField] Image[] shieldBars;
+    [SerializeField] GameObject gameOverContainer;
 
     int currentHealth;
     int deathCameraPriority = 20;
@@ -26,10 +27,16 @@ public class PlayerHealth : MonoBehaviour
         
         if (currentHealth <= 0)
         {
-            weaponCamera.parent = null; // WeaponCamera를 플레이어로부터 분리 -> 카메라가 사라지지 않도록 함
-            deathVirtualCamera.Priority = deathCameraPriority; // 죽으면 DeathCamera의 우선순위를 높여서 보이도록 함
-            Destroy(this.gameObject);
+            PlayerGameOver();
         }
+    }
+
+    void PlayerGameOver()
+    {
+        weaponCamera.parent = null; // WeaponCamera를 플레이어로부터 분리 -> 카메라가 사라지지 않도록 함
+        deathVirtualCamera.Priority = deathCameraPriority; // DeathCamera의 우선순위를 높여서 보이도록 함
+        gameOverContainer.SetActive(true); // 죽으면 게임오버 화면을 보여줌
+        Destroy(gameObject);
     }
 
     void AdjustShieldUI()
