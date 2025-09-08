0ac11b3430a207f6eea9a67abfdacef2c1df98a3
kimtaesung98 <xlavh25575@gmail.com>
2025-09-08 21:20:23 +0900

    187 game winner

--- Hyper_FPS/Assets/Scripts/Misc/GameManager.cs
+++ Hyper_FPS/Assets/Scripts/Misc/GameManager.cs
@@ -1,8 +1,27 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text enemiesLeftText;
+    [SerializeField] GameObject youWinText;
+
+    int enemiesLeft = 0;
+
+    const string ENEMIES_LEFT_STRING ="Enemeies Left: ";
+    public void AdjustenemiesLeft(int amount)
+    {
+        enemiesLeft += amount;
+        enemiesLeftText.text = ENEMIES_LEFT_STRING + enemiesLeft.ToString();
+
+
+        if (enemiesLeft <= 0)
+        {
+            youWinText.SetActive(true);
+        }
+    }
+
     public void RestartGame()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
